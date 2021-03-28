8e9353b099d6074371e29dc8b644667e7a1c5317
Lee Farnell <leefarnell@mac.com>
2021-03-28 15:08:14 +0100

    timer made functional and counting

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ const dateTime = document.getElementById("currentDay");
 $(document).ready(function () {
   console.log("Ready");
 
-  dateTime.append(moment().format("dddd Do MMMM, HH:mm:ss"));
+  function update() {
+    $("#currentDay").text(moment().format("dddd Do MMMM, HH:mm:ss"));
+  }
+
+  setInterval(update, 1000);
 });
