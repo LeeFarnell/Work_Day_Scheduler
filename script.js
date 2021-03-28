243f53f2c134ddc38014188ede7b589483a00da5
Lee Farnell <leefarnell@mac.com>
2021-03-28 14:39:02 +0100

    Linked Date/Time to p tags

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const bodyElement = document.body;
+const dateTime = document.getElementById("currentDay");
+
 $(document).ready(function () {
   console.log("Ready");
+
+  dateTime.append(moment().format("dddd Do MMMM, HH:mm:ss"));
 });
